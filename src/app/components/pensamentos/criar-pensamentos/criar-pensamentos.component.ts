805e560242f2301d1d7aa11f45a5a1f1231bd1a4
magnosansil <magnosansil@outlook.com>
2024-01-24 14:42:25 -0300

    Formulário reativo

--- src/app/components/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
+++ src/app/components/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamentos',
@@ -10,14 +11,11 @@ import { Router } from '@angular/router';
 })
 export class CriarPensamentoComponent implements OnInit {
 
-  pensamento: Pensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: 'modelo1'
-  }
+  formulario!: FormGroup
+
 
   criarPensamento() {
-    this.service.criar(this.pensamento).subscribe(() => {
+    this.service.criar(this.formulario.value).subscribe(() => {
       this.router.navigate(['/listarPensamento']);
     }
     )
@@ -33,10 +31,16 @@ export class CriarPensamentoComponent implements OnInit {
 
   constructor(
     private service: PensamentoService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
     ) { }
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['Formulário reativo'],
+      autoria: ['Angular'],
+      modelo: ['modelo1']
+    })
   }
 
 }
